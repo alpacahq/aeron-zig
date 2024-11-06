6472cbd29e0b2043273234eaa081685a57fc2717
Gabor Lekeny <gabor@alpaca.markets>
2024-11-06 23:19:01 +0100

    Fixes based on PR comments

--- build.zig
+++ build.zig
@@ -4,7 +4,7 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    if (target.result.cpu.arch != .x86_64) @panic("The target CPU architecture have to be 'x86_64'");
+    if (target.result.cpu.arch != .x86_64) @panic("The target CPU architecture needs to be 'x86_64'");
 
     const options = b.addOptions();
     const dynamic = b.option(bool, "dynamic", "link with dynamic library (default: false)") orelse false;
@@ -20,6 +20,6 @@ pub fn build(b: *std.Build) void {
     switch (target.result.abi) {
         .gnu => lib.linkSystemLibrary(if (dynamic) "aeron_libc" else "aeron_static_libc", .{}),
         .musl => lib.linkSystemLibrary(if (dynamic) "aeron_musl" else "aeron_static_musl", .{}),
-        else => @panic("The target ABI have to be 'musl' or 'gnu'"),
+        else => @panic("The target ABI needs to be 'musl' or 'gnu'"),
     }
 }
